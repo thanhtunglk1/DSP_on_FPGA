47c3970af22f2672095a980fb6f5bfcf00f007d8
Tung <thanhtunglk1@gmail.com>
2025-09-22 03:58:51 +0700

    Update top_module.sv

--- i2c/00_src/top_module.sv
+++ i2c/00_src/top_module.sv
@@ -14,7 +14,7 @@ module top_module (
     output logic        AUD_DACLRCK 
 );
 
-    logic config_done, config_synch_i, config_synch_done;
+    logic config_done;
 
     logic CLOCK_12, samp_tick_en;
 
@@ -30,23 +30,18 @@ module top_module (
         .io_sda (FPGA_I2C_SDAT)
     );
 
-    always_ff @(posedge CLOCK_12, negedge i_rst_n) begin : proc_synchronizer_config_done_i2c
-        if(~i_rst_n) {config_synch_done, config_synch_i} <= '0;
-        else         {config_synch_done, config_synch_i} <= {config_synch_i, config_done};
-    end
-
     assign AUD_XCK = CLOCK_12;
 
     codec_data codec_trans_rec (
-        .i_clk        (CLOCK_12),          //  12MHz - MCLK
+        .i_clk        (CLOCK_12),    //  12MHz - MCLK
         .i_rst_n      (SW[9]),
-        .i_config_done(config_synch_done), // done kick signal from i2c_config
-        .i_p2s_in     (p2s_dac),           // data need DAC
-        .o_s2p_out    (s2p_adc),           // only left channel
-        .o_bclk       (AUD_BLCK),          // 240kHz ( 40:60 - 0:1)
-        .o_daclrck    (AUD_DACLRCK),       //  48kHz ( 50:50 - 0:1) 
-        .o_adclrck    (AUD_ADCLRCK),       //  48kHz ( 50:50 - 0:1)   
-        .o_sample_tick(samp_tick_en),      //  48kHz (249: 1 - 0:1) 
+        .i_config_done(config_done), // done kick signal from i2c_config
+        .i_p2s_in     (p2s_dac),     // data need DAC
+        .o_s2p_out    (s2p_adc),     // only left channel
+        .o_bclk       (AUD_BLCK),    // 240kHz ( 40:60 - 0:1)
+        .o_daclrck    (AUD_DACLRCK), //  48kHz ( 50:50 - 0:1) 
+        .o_adclrck    (AUD_ADCLRCK), //  48kHz ( 50:50 - 0:1)   
+        .o_sample_tick(samp_tick_en),//  48kHz (249: 1 - 0:1) 
         .i_adc_dat    (AUD_ADCDAT),
         .o_dac_dat    (AUD_DACDAT)
     );
